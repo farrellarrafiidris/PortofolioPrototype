3005a9289242141883dd91e8cc31d35a5ad20cc3
farrellarrafidris <jshzhbsj@gmail.com>
2026-02-13 08:31:33 +0700

    adding new components

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: false, // Atau hapus baris ini sepenuhnya
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
